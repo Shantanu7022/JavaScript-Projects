249401c95d045d307b1a523700f84d56e62c72d4
Shantanu Gayen <shantanugayen200207@gmail.com>
2024-01-14 20:07:39 +0530

    commit changes

--- Rock-Paper-Scissors/script.js
+++ Rock-Paper-Scissors/script.js
@@ -9,11 +9,39 @@
     return options[randomIndex];
  };
 
+ const showWinner = (userWin) => {
+    if(userWin){
+        console.log("YOU Win!");
+    }else{
+        console.log("YOU Lose!");
+    }
+ }
+
+ const drawGame = () => {
+    console.log("Game was draw");
+ }
+
 const playGame = (userChoice) =>  {
     console.log("user choice = ", userChoice);
     //computer choice
     const compChoice = generateCompChoice();
     console.log("computer choice = ", compChoice);
+
+    if(userChoice === compChoice){
+        //game draw
+        drawGame();
+    }else{
+        let userWin = true;
+        if(userChoice === "rock"){
+            userWin = compChoice === "paper" ? false : true;  //paper or scissors
+        }else if(userChoice === "paper"){
+            userWin = compChoice === "rock" ? true : false;  //rock or scissors
+        }else{
+            userWin = compChoice === "rock" ? false : true;  //rock or paper
+        }
+
+        showWinner(userWin);
+    }
 };
 
  choices.forEach((choice) => {
